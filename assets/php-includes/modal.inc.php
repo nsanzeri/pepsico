ef08a5a83830c6fa4265dba1859d78aefdcd41c8
npsanze <npsanze@WN-ILA1MUAC6118.us.bank-dns.com>
2016-12-09 15:35:18 -0600

    Modal fixes.

--- assets/php-includes/modal.inc.php
+++ assets/php-includes/modal.inc.php
@@ -2,14 +2,23 @@
 			<div class="modal-dialog">
 					<div class="modal-content">
 							<div class="modal-header">
-									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-									<h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
+
+									<button type="button" class="close gp-closebtn" aria-hidden="true">x</button>
+									<h4 class="modal-title" id="myModalLabel">
+																	<?php
+								$source_str = '';
+								if( ($x_pos = strpos(getProductSearchCriteria(), ' | ')) !== FALSE ){
+									$source_str = substr(getProductSearchCriteria(), $x_pos + 3);
+								}
+								echo $source_str;
+								?>
+									</h4>
 							</div>
 							<div class="modal-body">
 								<div id="gp-modal-products"></div>
 							</div>
 							<div class="modal-footer">
-									<button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-default waves-effect gp-closebtn" data-dismiss="modal">Close</button>
 							</div>
 						</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
@@ -20,14 +29,22 @@
 			<div class="modal-dialog">
 					<div class="modal-content">
 							<div class="modal-header">
-									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-									<h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
+									<button type="button" class="close product-closebtn" data-dismiss="modal" aria-hidden="true">x</button>
+									<h4 class="modal-title" id="myModalLabel">
+										<?php
+										$source_str = '';
+										if( ($x_pos = strpos(getProductSearchCriteria(), ' | ')) !== FALSE ){
+											$source_str = substr(getProductSearchCriteria(), $x_pos + 3);
+										}
+										echo $source_str;
+										?>
+									</h4>
 							</div>
 							<div class="modal-body">
 								<div id="gp-modal-product"></div>
 							</div>
 							<div class="modal-footer">
-									<button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-default waves-effect product-closebtn" data-dismiss="modal">Close</button>
 							</div>
 						</div><!-- /.modal-content -->
 			</div><!-- /.modal-dialog -->
