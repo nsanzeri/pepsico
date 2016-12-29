eef666f5012f25e6f7c8359a25e54e712a7df226
Chris <Chris@Media13.fuseneo.com>
2016-12-29 08:43:15 -0600

    Changed "clear all" to "clear"

--- assets/php-includes/left-sidebar.inc.php
+++ assets/php-includes/left-sidebar.inc.php
@@ -2,7 +2,7 @@
                 <div class="sidebar-inner slimscrollleft">
                     <div class="user-details">
                         <img src="assets/images/logo_pepsico.png" alt="Logo" >
-                        <br><br><br>
+                        <br><br>
                         <h3>GLOBAL PACKAGING <span style="color: #03a9f4">DATABASE</span></h3>
                     </div>
                     <!--- Divider -->
@@ -17,27 +17,27 @@
                             </li>     
                             
                             <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-earth"></i> <span> Region </span> <span class="pull-right"><div class="gp-clearall" id="region-clear">clear all x</div></i></span></a>
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-earth"></i> <span> Region </span> <span class="pull-right"><div class="gp-clearall" id="region-clear">clear x</div></i></span></a>
                        				<?php echo buildFilter ( 'region', 'region_id' ); ?>        
                             </li>
 
                             <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-panorama-vertical"></i> <span> Format </span> <span class="pull-right"><div class="gp-clearall" id="format-clear">clear all x</div></i></span></a>
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-panorama-vertical"></i> <span> Format </span> <span class="pull-right"><div class="gp-clearall" id="format-clear">clear x</div></i></span></a>
 									<?php echo buildFilter ( 'format', 'format_id' ); ?>                                  
                             </li>
 
                             <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-copyright"></i><span> Brand </span><span class="pull-right"><div class="gp-clearall" id="brand-clear">clear all x</div></span></a>
+                                <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-copyright"></i><span> Brand </span><span class="pull-right"><div class="gp-clearall" id="brand-clear">clear x</div></span></a>
 									<?php echo buildFilter ( 'brand', 'brand_id' ); ?>
                             </li>
 
                             <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-crop"></i><span> Size </span><span class="pull-right"><div class="gp-clearall" id="size-clear">clear all x</div></i></span></a>
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-crop"></i><span> Size </span><span class="pull-right"><div class="gp-clearall" id="size-clear">clear x</div></i></span></a>
 									<?php echo buildFilter ( 'size', 'size_id' ); ?>
                             </li>
 
                             <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-database"></i><span> Finish </span><span class="pull-right"><div class="gp-clearall" id="finish-clear">clear all x</div></i></span></a>
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-database"></i><span> Finish </span><span class="pull-right"><div class="gp-clearall" id="finish-clear">clear x</div></i></span></a>
 									<?php echo buildFilter ( 'finish', 'finish_id' ); ?>                                
                             </li>
                         </ul>
