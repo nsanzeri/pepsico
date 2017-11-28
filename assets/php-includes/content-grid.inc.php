dcaea978eea91983b6e4d88a2545291c24852b04
Chris <Chris@Media13.fuseneo.com>
2017-11-28 08:12:50 -0600

    These were sitting unstaged for almost a year. Not sure why

--- assets/php-includes/content-grid.inc.php
+++ assets/php-includes/content-grid.inc.php
@@ -6,7 +6,7 @@
                 <h3 class="panel-title">Grid View</h3>
                 <p class="panel-title"><?php echo getProductCount(); ?><?php echo getProductSearchCriteria(); ?></p>
             </div>
-            <div class="panel-body">
+            <div class="panel-body-light">
 
                 <table id="datatable-buttons" class="table table-striped table-bordered">
 					<?php echo getProductsMatchingCriteria(); ?>
